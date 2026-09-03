5799f6aede35075a7eab3f58d0e1d2f26047b8fb
Vishal795-knightrider <vk3293801@gmail.com>
2026-09-03 23:24:30 +0530

    req and res server

--- 01 first node server/app.js
+++ 01 first node server/app.js
@@ -1,11 +1,11 @@
 const http=require('http');  //"Node.js, give me the HTTP module so that I can use its functions."
 
 const server=http.createServer((req,res)=>{  //yeh request ayegi server ke pass
-  console.log(req);    //yeh create server fn 1 object reurn krta hai aur us object ko humne sunna  hai
+  console.log(req);    //yeh createServer fn 1 object reurn krta hai aur us object ko humne sunna  hai
 })
 
 //yeh uper server bna to liya hai pr ab yeh request sunnni bhi to padegi serve
 
 server.listen(3000,()=>{     //server req ko sun raha  hai
   console.log("server sucessfully started");
-}); 
+});
