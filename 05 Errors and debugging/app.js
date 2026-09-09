27ae2f3823fe45076c8f4f3516d3b20ac18245d5
Vishal795-knightrider <vk3293801@gmail.com>
2026-09-09 22:00:28 +0530

    intilise new folder errors and debugging

---
+++ 05 Errors and debugging/app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const requestHandler = require('./syntax');
+
+const server = http.createServer((req,res)=>{
+  cos 
+})
+
+const PORT = 3001;
+server.listen(PORT, () => {
+  console.log(`Server running on address http://localhost:${PORT}`);
+});
