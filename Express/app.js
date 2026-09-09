b29520ac17aedf73ffaa166a453fb24bb5506ca4
Vishal795-knightrider <vk3293801@gmail.com>
2026-09-10 00:24:21 +0530

    Express installed

--- Express/app.js
+++ Express/app.js
@@ -1,1 +1,18 @@
 const express=require('express')
+
+// This creates an Express application/server object and stores it in app.
+const app=express();
+
+app.use((req,res,next)=>{
+  console.log("1 st middleware");
+  next();
+})
+
+app.use((req,res,next)=>{
+  console.log("2nd middleware");
+  res.send("its over");
+})
+
+app.listen(3000,()=>{
+  console.log("server okkk");
+})
