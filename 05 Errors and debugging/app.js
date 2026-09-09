9cd8dc60ec41280b900e1750d22f24693a8a021f
Vishal795-knightrider <vk3293801@gmail.com>
2026-09-09 22:23:04 +0530

    logical error

--- 05 Errors and debugging/app.js
+++ 05 Errors and debugging/app.js
@@ -1,11 +1,21 @@
 const http = require('http');
-const requestHandler = require('./syntax');
+const testingSyntax = require('./syntax')
+const runtime = require('./runtime')
+const logical = require('./logical')
 
-const server = http.createServer((req,res)=>{
-  cos 
-})
 
-const PORT = 3001;
+const requestHandler = require('./user');
+
+const server = http.createServer(requestHandler);
+
+// const server = http.createServer((req, res) => {
+//   console.log(req.url, req.method);
+//   //testingSyntax();
+//   // runtime();
+//   logical();
+// });
+
+const PORT = 3002;
 server.listen(PORT, () => {
   console.log(`Server running on address http://localhost:${PORT}`);
 });
